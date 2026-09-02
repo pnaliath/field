9cf9ce1cd58bf518ef152d2aab7814cfacaa5b84
pnaliath <125124762+pnaliath@users.noreply.github.com>
2026-09-02 18:11:49 +0530

    Use standard VST3 factory registration

--- experiments/vst-route-probe/source/factory.cpp
+++ experiments/vst-route-probe/source/factory.cpp
@@ -3,35 +3,33 @@
 #include "routeprobeprocessor.h"
 #include "version.h"
 
-#include "public.sdk/source/main/pluginfactory_constexpr.h"
+#include "public.sdk/source/main/pluginfactory.h"
 
 using namespace Steinberg;
 using namespace Steinberg::Vst;
 
 #define stringPluginName "Field Route Probe"
 
-BEGIN_FACTORY_DEF (stringCompanyName, stringCompanyWeb, stringCompanyEmail, 2)
+BEGIN_FACTORY_DEF (stringCompanyName, stringCompanyWeb, stringCompanyEmail)
 
-DEF_CLASS (FieldRouteProbe::ProcessorUID,
-           PClassInfo::kManyInstances,
-           kVstAudioEffectClass,
-           stringPluginName,
-           Vst::kDistributable,
-           "Fx|Analyzer",
-           FULL_VERSION_STR,
-           kVstVersionString,
-           FieldRouteProbe::Processor::createInstance,
-           nullptr)
+DEF_CLASS2 (INLINE_UID_FROM_FUID (FieldRouteProbe::ProcessorUID),
+            PClassInfo::kManyInstances,
+            kVstAudioEffectClass,
+            stringPluginName,
+            Vst::kDistributable,
+            "Fx|Analyzer",
+            FULL_VERSION_STR,
+            kVstVersionString,
+            FieldRouteProbe::Processor::createInstance)
 
-DEF_CLASS (FieldRouteProbe::ControllerUID,
-           PClassInfo::kManyInstances,
-           kVstComponentControllerClass,
-           stringPluginName " Controller",
-           0,
-           "",
-           FULL_VERSION_STR,
-           kVstVersionString,
-           FieldRouteProbe::Controller::createInstance,
-           nullptr)
+DEF_CLASS2 (INLINE_UID_FROM_FUID (FieldRouteProbe::ControllerUID),
+            PClassInfo::kManyInstances,
+            kVstComponentControllerClass,
+            stringPluginName " Controller",
+            0,
+            "",
+            FULL_VERSION_STR,
+            kVstVersionString,
+            FieldRouteProbe::Controller::createInstance)
 
 END_FACTORY
